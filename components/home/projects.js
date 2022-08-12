e8e6697e0bd0fb6f602323599926e2f1cfbde6f5
Kevin <krowens3rd@gmail.com>
2022-08-12 07:09:43 -0700

    ✅ Add missing styling

--- components/home/projects.js
+++ components/home/projects.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const Projects = () => {
+  const style = {
+    projectsHeaderWrapper: `py-20`,
+    headerContent: `ml-4 md:mx-16 grid grid-cols-12 gap-4 h-auto place-items-center items-center`,
+    textWrapper: `col-span-6 flex flex-col justify-center items-center`,
+    textContent: `items-center w-3/4 relative`,
+    boxVectorContainer: `block absolute right-0 top-0 w-20 w-52 opacity-60`,
+    title: `my-[6rem] text-1xl font-extrabold text-white leading-none`,
+    pinkAccent: `text-pink font-bold`,
+    textCardContainer: `relative col-span-6`,
+    projectsWrapper: `relative ml-4 md:mx-16`,
+    projectsContainer: `flex flex-col gap-4 place-items-center`,
+  }
   return <div></div>
 }
 
